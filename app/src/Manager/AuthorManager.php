8b7569627c88f59aac23606d4f6e6f768a3257d8
Patrick Chen <chen.patrick47@gmail.com>
2021-12-08 11:28:48 +0100

    added function authors manager

--- app/src/Manager/AuthorManager.php
+++ app/src/Manager/AuthorManager.php
@@ -23,6 +23,23 @@ class AuthorManager extends BaseManager
         ));
     }
 
+
+    /**
+
+     * @return Author[]
+     */
+    public function getAllAuthors()
+    {
+        $select = "SELECT * FROM authors";
+        $request = $this->bdd->prepare($select);
+        $request->fetchAll(PDO::FETCH_CLASS, 'Author');
+        return $request->fetchAll();
+    }
+
+   /**
+    * @param id
+     * @return Author
+     */
     public function getSingleAuthor($id)
     {
         $getAuthor = 'SELECT * FROM authors WHERE id = ?';
@@ -34,11 +51,47 @@ class AuthorManager extends BaseManager
 
     public function getUser($username)
     {
-        $getUser = 'SELECT * FROM Authors WHERE username = :username';
+        $getUser = 'SELECT * FROM Authors WHERE username = ?';
         $request = $this->bdd->prepare($getUser);
         $request->execute(array($username));
         $author = $request->fetch(PDO::FETCH_ASSOC);
         return new Author($author);
+    }
+
+       /**
+    * @param Author
+     * @return Boolean
+     */
+    public function updateAuthorNames($username, $firstname, $lastname, $id)
+    {
+        $update = "UPDATE authors SET username = :username, firstname = :firstname, lastname = :lastname WHERE id = :id";
+        $request = $this->bdd->prepare($update);
+        $request-> bindparam(':username', $username, PDO::PARAM_STR);
+        $request-> bindparam(':firstname', $firstname, PDO::PARAM_STR);
+        $request-> bindparam(':lastname', $lastname, PDO::PARAM_STR);
+        $request-> bindparam(':id', $id, PDO::PARAM_STR);
+        $request->execute(array(
+            'username' => $username,
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'id' => $id,
+        ));
+        return true;
+    }
+
+    /**
+    * @param id
+     * @return Boolean
+     */
+    public function deleteAuthor($id)
+    {
+        $delete = "DELETE FROM authors WHERE id = :id";
+        $request = $this->bdd->prepare($delete);
+        $request->bindParam(':id', $id, PDO::PARAM_STR);
+        $request->execute(array(
+            'id' => $id,
+        ));
+        return true;
     }
 
     public function checkCredential(string $username, string $password)
