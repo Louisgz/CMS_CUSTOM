cb9b4da6fdef3a32cc16887a7f93ac379e1e1bc1
Patrick Chen <chen.patrick47@gmail.com>
2021-12-08 17:04:46 +0100

    add url

--- app/src/Views/Frontend/home.php
+++ app/src/Views/Frontend/home.php
@@ -13,29 +13,33 @@
     ?>
   <div>
     <div>
-      <h3><?php echo $post['title'] ?></h3>
+        <a  href="/post?id=<?php echo $post['id'] ?>">
+            <h3><?php echo $post['title'] ?></h3>
+        </a>
       <p><?php echo $post['content'] ?></p>
     </div>
     <div>
+    <a  href="/post?id=<?php echo $post['id'] ?>">
       <p>
         commentaires:
       </p>
+      </a>
       <?php
-                foreach ($comments as $comment) {
-                    if ($comment['postId'] == $post['id']) {
-                ?>
-      <div>
-        <p>
-          <?php echo $comment['content'] ?>
-        </p>
-      </div>
-      <?php
-                    }
-                    ?>
+        foreach ($comments as $comment) {
+            if ($comment['postId'] == $post['id']) {
+            ?>
+                <div>
+                    <p>
+                    <?php echo $comment['content'] ?>
+                    </p>
+                </div>
+            <?php
+        }
+    ?>
 
       <?php
-                }
-                ?>
+        }
+        ?>
       <input type="text" id="commentInput" name="addComment" placeholder="ajouter commentaire">
       <button id="addComment" class="btn btn-primary" onclick="addComment(<?php echo $post['id'] ?>)">Ajouter</button>
     </div>
