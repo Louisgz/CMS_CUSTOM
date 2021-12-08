b0f1160f218c5ff3465bcc81cd4b752674f08299
Louis Pillon <louisgzan.pro@gmail.com>
2021-12-08 09:35:26 +0100

    Add comment entity

---
+++ app/src/Entity/Comment.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+class Comment extends BaseEntity
+{
+    private string $id;
+    private \DateTime $date;
+    private string $content;
+    private string $authorId;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return void
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getdate(): \DateTime
+    {
+        return $this->date;
+        return $this->date;
+    }
+
+    /**
+     * @return void
+     */
+    public function setDate(\DateTime $date): void
+    {
+        $this->id = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return void
+     */
+    public function setContent(string $content): void
+    {
+        $this->id = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorId(): string
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @return void
+     */
+    public function setAuthorId(string $authorId): void
+    {
+        $this->id = $authorId;
+    }
+}
