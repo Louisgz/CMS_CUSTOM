3d81f6a4e59139bea6ee1e67f61b4cc6557744e4
Louis Pillon <louisgzan.pro@gmail.com>
2021-12-09 15:47:22 +0100

    connect api for delete, create post

--- app/src/Views/Frontend/createPost.php
+++ app/src/Views/Frontend/createPost.php
@@ -3,7 +3,7 @@
 
     <h3 class="formTitle">Create a post</h3>
     <!-- Login Form -->
-    <form action="api/create-post" method='post'>
+    <form action="create-post" method='post'>
       <input type="text" id="title" name="title" placeholder="title">
       <textarea type="text" id="content" name="content" placeholder="content"></textarea>
       <input type="submit" value="Create post">
--- app/src/Views/Frontend/post.php
+++ app/src/Views/Frontend/post.php
@@ -1,5 +1,5 @@
-
 <?php
+
 /**
  * @var $user \App\Entity\Author
  * @var $posts \App\Entity\Post[]
@@ -8,61 +8,63 @@
 use App\Entity\Author;
 ?>
 <div class="flex-post-column">
-        <?php
-        foreach ($posts as $post) {
-            ?>
-            <div class="single-post-page">
-                <div class="single-post-background">                        
-                    <h3><?php echo $post['title'] ?></h3>
-                    <div>
-                        <button type="button" class="btn btn-danger">Delete post</button>
-                        <button type="button" class="btn btn-warning">Edit post</button>
-                    </div>
-                    <div>
-                        <p><?php echo $post['content'] ?></p>
-                    </div>
-                </div>
-                <div>
-                         <?php   if (isset($_SESSION['user'])){ ?>
-                            <div>
-                                <h4>Ajouter un commentaire :</h4>
-                            </div>
-                        <form action="create-comment/?id=<?php echo($_GET['id']) ?>" method='post'>
-                            <input type="text" id="commentInput" name="comment" placeholder="ajouter commentaire">
-                            <input class="btn btn-success" type="submit" value="add comment">
-                            
-                        </form>
+  <?php
+    foreach ($posts as $post) {
+    ?>
+  <div class="single-post-page">
+    <div class="single-post-background">
+      <h3><?php echo $post['title'] ?></h3>
+      <div>
+        <form action="delete-post?id=<?= $_GET['id'] ?>" method="post">
+          <button type="submit" class="btn btn-danger">Delete post</button>
+        </form>
+        <button type="button" class="btn btn-warning">Edit post</button>
+      </div>
+      <div>
+        <p><?php echo $post['content'] ?></p>
+      </div>
+    </div>
+    <div>
+      <?php if (isset($_SESSION['user'])) { ?>
+      <div>
+        <h4>Ajouter un commentaire :</h4>
+      </div>
+      <form action="create-comment/?id=<?= $_GET['id'] ?>" method='post'>
+        <input type="text" id="commentInput" name="comment" placeholder="ajouter commentaire">
+        <input class="btn btn-success" type="submit" value="add comment">
 
-                       <?php }?>
-                    </div>
-                    <div class="single-post-background">
-                    <p>
-                    commentaires:
-                    </p>
-                    <?php
-                    foreach ($comments as $comment){
-                        if($comment['postId'] == $post['id']){
-                            ?>
-                            <div>
-                                <div>
-                                    <p>
-                                        <?php echo $comment['content'] ?>
-                                    </p>
-                                </div>
-                                <button type="button" class="btn btn-danger">Delete post</button>
-                            </div>
-                            <?php
-                        }
-                        ?>
-                        
-                        <?php
+      </form>
+
+      <?php } ?>
+    </div>
+    <div class="single-post-background">
+      <p>
+        commentaires:
+      </p>
+      <?php
+                foreach ($comments as $comment) {
+                    if ($comment['postId'] == $post['id']) {
+                ?>
+      <div>
+        <div>
+          <p>
+            <?php echo $comment['content'] ?>
+          </p>
+        </div>
+        <button type="button" class="btn btn-danger">Delete post</button>
+      </div>
+      <?php
                     }
                     ?>
-                </div>
-            </div>
-        <?php
-        }
-        
-        ?>
-      
+
+      <?php
+                }
+                ?>
+    </div>
+  </div>
+  <?php
+    }
+
+    ?>
+
 </div>
