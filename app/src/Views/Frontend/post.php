aafa462b6caf42b8c34670acf7c2b1dfa439094b
Louis Pillon <louisgzan.pro@gmail.com>
2021-12-10 14:14:05 +0100

    add databse to main

--- app/src/Views/Frontend/post.php
+++ app/src/Views/Frontend/post.php
@@ -14,33 +14,33 @@ use App\Entity\Author;
   <div class="single-post-page">
     <div class="single-post-container">
       <div style="display: flex; flex-direction: row; justify-content: center">
-      <div>
-        <h2><?php echo $post['title'] ?></h2>
-        <?php
-          $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-          if ($user && ($user['isAdmin'] == 1 || $user['id'] === $post['authorId'])) {
-          ?>
-        <div class="button-flex" style="margin: 1rem 0 3rem">
-          <form action="delete-post?id=<?= $_GET['id'] ?>" method="post" style='margin-right: 1.5rem'>
-            <button type="submit" class="btn btn-danger">Delete post</button>
-          </form>
-          </form>
+        <div>
+          <h2><?php echo $post['title'] ?></h2>
+          <?php
+            $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
+            if ($user && ($user['isAdmin'] == 1 || $user['id'] === $post['authorId'])) {
+            ?>
+          <div class="button-flex" style="margin: 1rem 0 3rem">
+            <form action="delete-post?id=<?= $_GET['id'] ?>" method="post" style='margin-right: 1.5rem'>
+              <button type="submit" class="btn btn-danger">Delete post</button>
+            </form>
+            </form>
+            <div>
+              <a href="/edit-post?id=<?= $_GET['id'] ?>">
+                <button type="button" class="btn btn-warning">Edit post</button>
+              </a>
+            </div>
+          </div>
+          <?php
+            }
+            ?>
           <div>
-            <a href="/edit-post?id=<?= $_GET['id'] ?>">
-              <button type="button" class="btn btn-warning">Edit post</button>
-            </a>
+            <p><?php echo $post['content'] ?></p>
           </div>
         </div>
-        <?php
-          }
-          ?>
         <div>
-          <p><?php echo $post['content'] ?></p>
-        </div>    
-      </div>
-      <div>
-        <img src="<?php echo $post['image'] ?>" alt="">
-      </div>
+          <img src="<?php echo $post['image'] ?>" alt="">
+        </div>
       </div>
     </div>
 
@@ -76,7 +76,7 @@ use App\Entity\Author;
         <div class="button-flex">
           <?php
                 $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-                if ($user && ($user['isAdmin'] === 1 || $user['id'] === $comment['authorId'])) {
+                if ($user && ($user['isAdmin'] == 1 || $user['id'] === $comment['authorId'])) {
                 ?>
           <form action="delete-comment?id=<?= $comment['id'] ?>&postId=<?= $post['id'] ?>" method='post'
             style="margin-right: 1rem">
